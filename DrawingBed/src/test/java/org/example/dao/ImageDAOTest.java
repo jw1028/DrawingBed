f8331ec7214a799dc1b5b379e26f6d6d21f9de5b
jw1028 <1711982870@qq.com>
2021-06-23 13:23:37 +0800

    图床

---
+++ DrawingBed/src/test/java/org/example/dao/ImageDAOTest.java
@@ -0,0 +1,59 @@
+package org.example.dao;
+
+import org.example.model.Image;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 单元测试
+ */
+public class ImageDAOTest {
+
+    @Test
+    public void queryCountByMd5() {
+        ImageDAO imageDAO = new ImageDAO();
+        int n = imageDAO.queryCountByMd5("c4329f0beed8da1e59c2ba25a35226c3");
+        System.out.println(n);
+    }
+
+    @Test
+    public void insert() {
+        ImageDAO imageDAO = new ImageDAO();
+        Image image = new Image();
+        image.setImageName("7.jpg");
+        image.setSize((long)52074);
+        image.setUploadTime("2021-06-22 00:00:14");
+        image.setMd5("c4329f0beed8da1e59c2ba25a35226c3");
+        image.setContentType("image/jpeg");
+        image.setPath("/c4329f0beed8da1e59c2ba25a35226c3");
+        imageDAO.insert(image);
+    }
+
+    @Test
+    public void queryAllImage() {
+        ImageDAO imageDAO = new ImageDAO();
+        List<Image> list = new ArrayList<>();
+        list = imageDAO.queryAllImage();
+        System.out.println(list.size());
+        for(Image image : list) {
+            System.out.println(image.getImageName());
+        }
+    }
+
+    @Test
+    public void queryOneImage() {
+        ImageDAO imageDAO = new ImageDAO();
+        Image image = imageDAO.queryOneImage(5);
+        System.out.println(image);
+
+    }
+
+    @Test
+    public void delete() {
+        ImageDAO imageDAO = new ImageDAO();
+        int n = imageDAO.delete(8);
+        System.out.println(n);
+    }
+}
